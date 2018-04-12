291a237070c26b43e779cd9a73209ea7fd845eaf
WORKPC\Ralph_Fonz <raphael@fonseca.tech>
2018-04-12 09:09:05 -0400

    Added get port and scale weight

--- ScaleServiceStackConsole.ServiceInterface/MyServices.cs
+++ ScaleServiceStackConsole.ServiceInterface/MyServices.cs
@@ -6,9 +6,25 @@ namespace ScaleServiceStackConsole.ServiceInterface
 {
     public class MyServices : Service
     {
+        //================== { Test } ==================//
+
         public object Any(Hello request)
         {
             return new HelloResponse { Result = $"Hello, {request.Name}!" };
         }
+        //================= { Port Info } ================//
+
+        public object Any(PortInfo request)
+        {
+            return new PortInfoReturn { PortResult = $"{request.Data}" };
+        }
+
+        //=============== { Scale Weight }==============//
+
+        public object Any(ScaleWeight request)
+        {
+            return new ScaleWeightResponse { ScaleDataResult = $"{ request.Data }" };
+        }
+
     }
 }
--- ScaleServiceStackConsole.ServiceModel/Hello.cs
+++ ScaleServiceStackConsole.ServiceModel/Hello.cs
@@ -2,6 +2,7 @@
 
 namespace ScaleServiceStackConsole.ServiceModel
 {
+    //================== { Test } ==================//
     [Route("/hello")]
     [Route("/hello/{Name}")]
     public class Hello : IReturn<HelloResponse>
@@ -13,4 +14,36 @@ namespace ScaleServiceStackConsole.ServiceModel
     {
         public string Result { get; set; }
     }
+    //================= { Port Info } ================//
+    [Route("/PortInfo/{Data}", Verbs = "GET")]
+
+    public class PortInfo: IReturn<PortInfoReturn>
+    {
+       public string Data { get; set; }
+    }
+    public class PortInfoReturn
+    {
+        public string PortResult { get; set; }
+    }
+
+    //=============== { Scale Weight } ==============//
+    [Route("/ScaleWeight")]
+    [Route("/ScaleWeight/{Data}")]
+    public class ScaleWeight: IReturn<ScaleWeightResponse>
+    {
+        public string Data { get; set; }
+    }
+
+    public class ScaleWeightResponse
+    {
+        public string ScaleDataResult { get; set; }
+    }
+
+    //=============== { Scale Weight } ==============//
+
+
+
+
+
+
 }
